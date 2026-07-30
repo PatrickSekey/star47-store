87cccb119db281bee9d6f3d90e43b4c11be74152
Patrick Wah Sekey <patrick.sekey.ps@gmail.com>
2026-07-30 11:25:30 +0530

    Add complete React frontend with package.json and dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { CartProvider } from './context/CartContext';
+import logo from './logo.svg';
 import './App.css';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-import Fashion from './pages/Fashion';
-import Checkout from './pages/Checkout';
 
 function App() {
   return (
-    <CartProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/fashion/:productType/:designId" element={<Fashion />} />
-            <Route path="/checkout" element={<Checkout />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </CartProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
